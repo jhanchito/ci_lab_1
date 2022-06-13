80eccd34dc27e286e3c7532f9b03eb19fbfab9d3
jhanchito <42454129+jhanchito@users.noreply.github.com>
2022-06-12 22:05:31 -0500

    coverage

--- src/wallet.py
+++ src/wallet.py
@@ -1,3 +1,18 @@
+class InsufficientAmount(Exception):
+    pass
 
-def test_wallet_spend_cash(wallet):
-    wallet.spemd
+
+class Wallet(object):
+
+    def __init__(self, initial_amount=0):
+        self.balance = initial_amount
+
+    def spend_cash(self, amount):
+        if self.balance < amount:
+            raise InsufficientAmount(
+                'Not enough available to spend {}'.format(amount)
+            )
+        self.balance -= amount
+
+    def add_cash(self, amount):
+        self.balance += amount
